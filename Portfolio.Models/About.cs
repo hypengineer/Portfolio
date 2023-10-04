bc4af87df51e858cc8c46eeb9af93da442351e3d
Hypengineer <okanizi16@gmail.com>
2023-10-04 23:16:50 +0300

    About modeli oluşturuldu

---
+++ Portfolio.Models/About.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portfolio.Models
+{
+    public class About
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime? Birthday { get; set; }
+        public int? Age { get; set; }
+        public string? Degree { get; set; }
+        public string? Phone { get; set; }
+        public string? Email { get; set; }
+        public string? City { get; set; }
+        public string? ImageUrl { get; set; }
+
+    }
+}
--- Portfolio.Web/Controllers/HomeController.cs
+++ Portfolio.Web/Controllers/HomeController.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Portfolio.Web.Models;
 using System.Diagnostics;
 
 namespace Portfolio.Web.Controllers
@@ -18,15 +17,7 @@ namespace Portfolio.Web.Controllers
             return View();
         }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
+      
+       
     }
 }
